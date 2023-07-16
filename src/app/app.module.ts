ca56e6c4c43954e45427868c8ce210d5405b1d0d
bercalinidev <alisson.develooper@hotmail.com]>
2023-07-16 03:54:28 -0300

    implementação do front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,71 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Trabalhar com angular com formularios
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// trabalhar com requisiões http
+import { HttpClientModule } from '@angular/common/http';
+
+// componentes do projeto
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
+
+//componentes do material
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {ToastrModule} from 'ngx-toastr';
+
+
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    HeaderComponent,
+    TecnicoListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    HttpClientModule,
+
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatButtonModule,
+    
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true
+    })
   ],
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Credenciais } from '../models/credenciais';
+import { API_CONFIG } from '../config/api.config';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  jwtService: JwtHelperService = new JwtHelperService();
+
+  constructor(private client: HttpClient) { }
+
+  authentication(credenciais: Credenciais) {
+    return this.client.post(`${API_CONFIG.baseUrl}/login`, credenciais, {
+      observe: 'response',
+      responseType: 'text'
+    });
+  }
+
+  loginComSucesso(authToken: string) {
+    localStorage.setItem('token', authToken);
+  }
+
+  isAuthentication() {
+    let token = localStorage.getItem('token');
+    if(token != null) {
+      return !this.jwtService.isTokenExpired(token);
+    }
+    return false;
+  }
+
+  logout() {
+    localStorage.clear();
+  }
+}
